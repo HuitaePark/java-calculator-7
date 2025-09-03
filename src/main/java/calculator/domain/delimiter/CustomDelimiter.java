6b202ee6fbc169a2594413f780ee0287303a5453
HuitaePark <qkrgmlxo3174@gmail.com>
2025-09-04 00:20:32 +0900

    fix: 커스텀 구분자일때 문자열 안자르던 문제 수정

--- src/main/java/calculator/domain/delimiter/CustomDelimiter.java
+++ src/main/java/calculator/domain/delimiter/CustomDelimiter.java
@@ -12,7 +12,12 @@ public class CustomDelimiter implements Delimiter {
 
     @Override
     public List<String> separateString(String text) {
+        text = splittingText(text);
         return List.of(text.split(delimiter));
     }
 
+    private String splittingText(String text) {
+        int beginIndex = 6;
+        return text.substring(beginIndex);
+    }
 }
